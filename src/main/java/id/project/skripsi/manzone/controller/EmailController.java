d3a66cb01ef4b758d06632d2c6b20167e5fbdef8
unknown <maderajaadi@gmail.com>
2020-03-19 11:57:58 +0700

    add unit test and update functionality for sending email

--- src/main/java/id/project/skripsi/manzone/controller/EmailController.java
+++ src/main/java/id/project/skripsi/manzone/controller/EmailController.java
@@ -1,17 +1,21 @@
 package id.project.skripsi.manzone.controller;
 
-import id.project.skripsi.manzone.dto.response.Response;
+import com.java.auth.dto.Response;
 import id.project.skripsi.manzone.service.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
-import static id.project.skripsi.manzone.constant.AppConstant.*;
+
+import static com.java.auth.constant.ResponseConstant.OK;
+
 
 @RestController
-@RequestMapping("/test")
+@RequestMapping("/v1")
 public class EmailController {
 
     final EmailService emailService;
@@ -23,12 +27,7 @@ public class EmailController {
 
     @GetMapping("/getEmail")
     public ResponseEntity sendFeedBack(HttpServletResponse response){
-        try {
             String success = emailService.getEmail();
-            return new ResponseEntity(new Response(false,response.getStatus(),STATUS_200.getMessage(),"The Email Has Been Sent. Please Check Your Email for Verification Code!"), HttpStatus.OK);
-        }catch(Exception e){
-            return new ResponseEntity(new Response(true,response.getStatus(),"There is an error in our server: {}" + e.getMessage(),null),HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+            return new ResponseEntity(new Response(false,response.getStatus(),OK.getMessage(),success), HttpStatus.OK);
     }
-
 }
--- src/main/java/id/project/skripsi/manzone/service/impl/EmailServiceImpl.java
+++ src/main/java/id/project/skripsi/manzone/service/impl/EmailServiceImpl.java
@@ -3,6 +3,8 @@ package id.project.skripsi.manzone.service.impl;
 import id.project.skripsi.manzone.config.EmailConfig;
 import id.project.skripsi.manzone.service.EmailService;
 import id.project.skripsi.manzone.service.OtpService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
@@ -10,6 +12,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class EmailServiceImpl implements EmailService {
+
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmailServiceImpl.class.getName());
 
     final EmailConfig emailConfig;
     final OtpService otpService;
@@ -24,22 +29,27 @@ public class EmailServiceImpl implements EmailService {
     public String getEmail() {
 
         //create mail sender
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setPort(this.emailConfig.getPort());
-        mailSender.setHost(this.emailConfig.getHost());
-        mailSender.setUsername(this.emailConfig.getUsername());
-        mailSender.setPassword(this.emailConfig.getPassword());
+        try {
+            JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+            mailSender.setPort(this.emailConfig.getPort());
+            mailSender.setHost(this.emailConfig.getHost());
+            mailSender.setUsername(this.emailConfig.getUsername());
+            mailSender.setPassword(this.emailConfig.getPassword());
 
-        //create email instance
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
-        mailMessage.setFrom("admin@gmail.com");
-        mailMessage.setTo("maderajaadi@gmail.com");
-        mailMessage.setSubject("Verify Your Account Now!");
-        mailMessage.setText("Your verification code is "+ otpService.createOtpCode() + " \n" + "If this verification code doesn't work, please contact our admin at admin@gmail.com");
+            //create email instance
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom("admin@gmail.com");
+            mailMessage.setTo("maderajaadi@gmail.com");
+            mailMessage.setSubject("Verify Your Account Now!");
+            mailMessage.setText("Your verification code is " + otpService.createOtpCode() + " \n" + "If this verification code doesn't work, please contact our admin at admin@gmail.com");
 
 
-        //send email with mail sender
-        mailSender.send(mailMessage);
-        return "Your Email has Been Sent. Please Check Your Mailbox To Complete Your Registration Process!";
+            //send email with mail sender
+            mailSender.send(mailMessage);
+            return "Your Email has Been Sent. Please Check Your Mailbox To Complete Your Registration Process!";
+        }catch(Exception e){
+            LOGGER.info("there is an error while sending an email: {}", e.getMessage());
+            return null;
+        }
     }
 }
